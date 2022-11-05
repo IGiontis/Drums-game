8b4af3bf2853a7f4304c8df232ad48b503c08e4d
IGiontis <iliasgiontis@gmail.com>
2022-11-05 17:43:41 +0200

    working sound

--- js/script.js
+++ js/script.js
@@ -13,11 +13,14 @@ const box3 = document.querySelector(".box-3");
 const box4 = document.querySelector(".box-4");
 const box5 = document.querySelector(".box-5");
 
+// Sounds
+
 // Working start / info buttons
 const startGame = function () {
   btnStart.addEventListener("click", function (e) {
     e.preventDefault();
     boxContainer.classList.remove("hidden");
+    keyPress();
   });
 };
 
@@ -30,49 +33,63 @@ const infoButton = function () {
 
 //Working on boxes and buttons
 
-allBoxes.forEach((value) => {
-  //   console.log(value);
-  value.addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log(e);
-  });
-});
+// allBoxes.forEach((value) => {
+//   //   console.log(value);
+//   value.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     console.log(e);
+//   });
+// });
 
 const keyPress = function () {
   window.addEventListener("keydown", function (e) {
     if (e.key === "a" || e.key === "A") {
-      box1.style.backgroundColor = "red";
-      this.setInterval(() => {
-        box1.style.backgroundColor = "white";
-      }, 400);
+      const sound1 = new Audio("./sounds/crash.mp3");
+      box1.classList.add("pressed");
+
+      sound1.play();
+      setInterval(() => {
+        box1.classList.remove("pressed");
+      }, 900);
     }
     if (e.key === "w" || e.key === "W") {
-      box2.style.backgroundColor = "red";
-      this.setInterval(() => {
-        box2.style.backgroundColor = "white";
-      }, 400);
+      const sound2 = new Audio("./sounds/crash.mp3");
+      box2.classList.add("pressed");
+
+      sound2.play();
+      setInterval(() => {
+        box2.classList.remove("pressed");
+      }, 900);
     }
     if (e.key === "s" || e.key === "S") {
-      box3.style.backgroundColor = "red";
-      this.setInterval(() => {
-        box3.style.backgroundColor = "white";
-      }, 400);
+      const sound3 = new Audio("./sounds/crash.mp3");
+      box3.classList.add("pressed");
+
+      sound3.play();
+      setInterval(() => {
+        box3.classList.remove("pressed");
+      }, 900);
     }
     if (e.key === "d" || e.key === "D") {
-      box4.style.backgroundColor = "red";
-      this.setInterval(() => {
-        box4.style.backgroundColor = "white";
-      }, 400);
+      const sound4 = new Audio("./sounds/crash.mp3");
+      box4.classList.add("pressed");
+
+      sound4.play();
+      setInterval(() => {
+        box4.classList.remove("pressed");
+      }, 900);
     }
     if (e.key === "z" || e.key === "Z") {
-      box5.style.backgroundColor = "red";
-      this.setInterval(() => {
-        box5.style.backgroundColor = "white";
-      }, 400);
+      const sound5 = new Audio("./sounds/crash.mp3");
+      box5.classList.add("pressed");
+
+      sound5.play();
+      setInterval(() => {
+        box5.classList.remove("pressed");
+      }, 900);
     }
   });
 };
 
-keyPress();
 infoButton();
 startGame();
